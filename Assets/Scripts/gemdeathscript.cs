122828fca65e50f1cc50db9cf56c8ed4e0b8e6ae
RealFrikas <kastanasfivos@gmail.com>
2025-02-07 18:24:28 +0200

    hi

--- Assets/Scripts/gemdeathscript.cs
+++ Assets/Scripts/gemdeathscript.cs
@@ -7,12 +7,26 @@ using UnityEngine;
 
 public class gem1script : MonoBehaviour
 {
-
-
-
+    public FirstPersonController playerscript;
+    public int gemid;
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        switch(gemid)
+        {
+            //find powerups
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+        playerscript.MoveSpeed = 8.0f;
     }
 }
